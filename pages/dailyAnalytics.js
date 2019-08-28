fff2f3d5deae8d79d355afca47f6c235af6e21eb
Angelico <angelico.delosreyes@gmail.com>
2019-08-28 10:51:21 +0800

    upadte so and so

---
+++ pages/dailyAnalytics.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+// const  DailyAnalytics = () => (
+const sayHelloWorld = () => {
+  return (
+    <Layout>
+      <h1>what are you doing here?</h1>
+    </Layout>
+  )
+}
+// )
+
+export default class DailyAnalytics extends React.Component{
+    sayHelloWorld(){
+        return (
+            <Layout>
+            <h1>Hello there</h1>
+          </Layout>
+        )
+    }
+
+
+
+    render(){
+        return this.sayHelloWorld()
+    }
+}
+
+
+
+
+

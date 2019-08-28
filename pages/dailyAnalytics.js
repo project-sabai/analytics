5bb69e290a358447b94ef47605198b1277fe70a1
Angelico <angelico.delosreyes@gmail.com>
2019-08-28 13:03:22 +0800

    changes done at 1pm

--- pages/dailyAnalytics.js
+++ pages/dailyAnalytics.js
@@ -15,7 +15,7 @@ export default class DailyAnalytics extends React.Component{
     sayHelloWorld(){
         return (
             <Layout>
-            <h1>Hello there</h1>
+            <h1>Hello there, how are you?</h1>
           </Layout>
         )
     }
